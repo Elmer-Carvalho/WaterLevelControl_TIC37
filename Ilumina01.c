98b122db89723e66ba814e32dbb64702023746e5
Bia Lima <128002730+biaslima@users.noreply.github.com>
2025-06-11 15:44:28 -0300

    Leitura do potenciometro e acionamento da bomba funcionais

--- Ilumina01.c
+++ Ilumina01.c
@@ -1,14 +1,3 @@
-/*
- * Por: Wilton Lacerda Silva
- *    
- * Monitoramento de Iluminação com ADC no Raspberry Pi Pico W (BitDogLab)
- *
- *  Este projeto demonstra o uso do conversor analógico-digital (ADC) do Raspberry 
- *  Pi Pico W na placa BitDogLab para monitorar níveis de iluminação ambiente em 
- *  tempo real. A leitura é exibida graficamente em um display OLED SSD1306 via I2C.
- * 
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "pico/stdlib.h"
@@ -16,11 +5,16 @@
 #include "hardware/i2c.h"
 #include "lib/ssd1306.h"
 #include "lib/font.h"
+
 #define I2C_PORT i2c1
 #define I2C_SDA 14
 #define I2C_SCL 15
 #define endereco 0x3C
 #define ADC_PIN 28  // GPIO compartilhada com o microfone na BitDogLab.
+#define RELAY_PIN 8
+
+#define LIM_MIN 30.0
+#define LIM_MAX 70.0
 
 
 //Trecho para modo BOOTSEL com botão B
@@ -40,56 +34,64 @@ int main()
   gpio_set_irq_enabled_with_callback(botaoB, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
   //Aqui termina o trecho para modo BOOTSEL com botão B
 
-  // I2C Initialisation. Using it at 400Khz.
+  // I2C Initialisation
   i2c_init(I2C_PORT, 400 * 1000);
+  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); 
+  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); 
+  gpio_pull_up(I2C_SDA); 
+  gpio_pull_up(I2C_SCL); 
 
-  gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-  gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Set the GPIO pin function to I2C
-  gpio_pull_up(I2C_SDA); // Pull up the data line
-  gpio_pull_up(I2C_SCL); // Pull up the clock line
-  ssd1306_t ssd; // Inicializa a estrutura do display
-  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+  // Inicializa o display
+  ssd1306_t ssd; 
+  ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); 
   ssd1306_config(&ssd); // Configura o display
   ssd1306_send_data(&ssd); // Envia os dados para o display
 
-  // Limpa o display. O display inicia com todos os pixels apagados.
   ssd1306_fill(&ssd, false);
   ssd1306_send_data(&ssd);
 
+  // Incializa o ADC
   adc_init();
-  adc_gpio_init(ADC_PIN); // GPIO 28 como entrada analógica
+  adc_gpio_init(ADC_PIN); 
 
+
+  // Inicializa o pino do relé
+  gpio_init(RELAY_PIN);
+  gpio_set_dir(RELAY_PIN, GPIO_OUT); // Define o pino como saída
+  gpio_put(RELAY_PIN, 1); 
 
   uint16_t adc_value_x;
-  float ldr_pcento;
-  char str_x[5];  // Buffer para armazenar a string
-  char str_y[10]; // Buffer para armazenar a string
+  float nivel_percentual;
+  char buffer_adc[10];  // Buffer para armazenar a string
+  char buffer_nivel[10]; // Buffer para armazenar a string
+  bool bomba_ligada = false;
   
   bool cor = true;
   while (true)
   {
-    adc_select_input(2); // O pino 28 como entrada analógica
-    adc_value_x = adc_read(); // Faz leitura do LRD
-    ldr_pcento = (adc_value_x * 100) / 4095; // Converte em porcentagem
-   
-    sprintf(str_x, "%d", adc_value_x);  // Converte o inteiro em string
-    sprintf(str_y, "%1.0f", 100-ldr_pcento);  // Converte o float em string
+    adc_select_input(2); 
+    adc_value_x = adc_read(); 
+    nivel_percentual = (adc_value_x * 100) / 4095; // Converte em porcentagem
+
+     ssd1306_fill(&ssd, !cor); 
+    if (nivel_percentual < LIM_MIN && !bomba_ligada){
+      gpio_put(RELAY_PIN, 0); // Liga o relé
+      bomba_ligada = true;
+    } else if(nivel_percentual > LIM_MAX && bomba_ligada){
+      gpio_put(RELAY_PIN, 1); 
+      bomba_ligada = false;
+    }
     
-    //cor = !cor;
-    // Atualiza o conteúdo do display com animações
-    ssd1306_fill(&ssd, !cor); // Limpa o display
+    sprintf(buffer_adc, "%d", adc_value_x);  // Converte o inteiro em string
+    sprintf(buffer_nivel, "%1.0f", nivel_percentual);  // Converte o float em string
+    // Atualiza o conteúdo do display
     ssd1306_rect(&ssd, 3, 3, 122, 60, cor, !cor); // Desenha um retângulo
-    ssd1306_line(&ssd, 3, 25, 123, 25, cor); // Desenha uma linha
-    ssd1306_line(&ssd, 3, 37, 123, 37, cor); // Desenha uma linha   
-    ssd1306_draw_string(&ssd, "CEPEDI   TIC37", 8, 6); // Desenha uma string
-    ssd1306_draw_string(&ssd, "EMBARCATECH", 20, 16); // Desenha uma string
-    ssd1306_draw_string(&ssd, "  Iluminacao", 10, 28); // Desenha uma string 
-    ssd1306_draw_string(&ssd, "ADC", 13, 41); // Desenha uma string    
-    ssd1306_draw_string(&ssd, "Nivel LUZ", 50, 41); // Desenha uma string    
-    ssd1306_line(&ssd, 44, 37, 44, 60, cor); // Desenha uma linha vertical         
-    ssd1306_draw_string(&ssd, str_x, 8, 52); // Desenha uma string     
-    ssd1306_draw_string(&ssd, str_y, 75, 52); // Desenha uma string   
-    ssd1306_send_data(&ssd); // Atualiza o display
+    ssd1306_draw_string(&ssd, "Nivel de Agua:", 8, 6); 
+    ssd1306_draw_string(&ssd, buffer_nivel, 8, 22); 
+    ssd1306_draw_string(&ssd, "ADC:", 8, 41); 
+    ssd1306_draw_string(&ssd, buffer_adc, 40, 41);  
+    ssd1306_draw_string(&ssd, bomba_ligada ? "Bomba: ON" : "Bomba: OFF", 8, 52);  
+    ssd1306_send_data(&ssd); 
     sleep_ms(700);
   }
 }
